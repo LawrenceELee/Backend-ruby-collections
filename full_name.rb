3a190609f37d97531d663bdd4809e4c89fd64f7a
Lawrence L <Lawrence.E.Lee@gmail.com>
2021-11-01 17:31:59 -0700

    Added prompts to questions

--- full_name.rb
+++ full_name.rb
@@ -2,13 +2,18 @@
 (1) Write a program which asks for a person's first name, then middle, then last. It should store each of these parts in an array. Finally, it should greet the person using their full name. Call the program full_name.rb.
 =end
 
+#names array
+names = Array.new()
+
 puts "What is your first name?"
-fname = gets.chomp
+names.push(gets.chomp)
 
 puts "What is your middle name?"
-mname = gets.chomp
+names.push(gets.chomp)
 
 puts "What is your last name?"
-lname = gets.chomp
+names.push(gets.chomp)
 
-puts "Hello" 
+print "Hello "
+names.each{|n| print n, " "}
+puts ""
