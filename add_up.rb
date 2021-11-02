4cae68346d3aac6aa523e61c94dc96cbd1d09c97
Lawrence L <Lawrence.E.Lee@gmail.com>
2021-11-01 17:35:39 -0700

    Wrote the psuedo code

--- add_up.rb
+++ add_up.rb
@@ -1,3 +1,11 @@
 =begin
 Write a program with a function add_up(i) . It is to be passed a positive integer, and it will add all the numbers from 1 to that integer and return the sum. Call the function three times within the program, and each time print out the return value. Call the program add_up.rb.
 =end
+
+# prompt user for 2 positive numbers
+
+# add the numbers from 1 to X in a for loop (write as a function)
+
+# loop that function 3 times
+
+# return value
--- sorted_words.rb
+++ sorted_words.rb
@@ -1,3 +1,11 @@
 =begin
 Write a program called sorted_words.rb. It should prompt the user for words and add each to an array. The user should be able to add as many words as they like, until they just hit enter to return a blank word. Then sort the array using the sort method and print it out.
 =end
+
+# prompt user for input in a while loop until "enter" or blank
+
+
+# use the built-in sort function
+
+
+# output array
