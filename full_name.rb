129bdb87490efd0d31ef07f7ecda5eb9aa03f497
Lawrence L <Lawrence.E.Lee@gmail.com>
2021-11-01 17:19:35 -0700

    Finished full_name.rb

--- full_name.rb
+++ full_name.rb
@@ -1,1 +1,14 @@
+=begin
+(1) Write a program which asks for a person's first name, then middle, then last. It should store each of these parts in an array. Finally, it should greet the person using their full name. Call the program full_name.rb.
+=end
 
+puts "What is your first name?"
+fname = gets.chomp
+
+puts "What is your middle name?"
+mname = gets.chomp
+
+puts "What is your last name?"
+lname = gets.chomp
+
+puts "Hello" 
