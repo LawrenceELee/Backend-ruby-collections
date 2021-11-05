8091ac605dd60e11d5b67ac232c715f4f4a058eb
Lawrence L <Lawrence.E.Lee@gmail.com>
2021-11-04 22:15:23 -0700

    Finished leap_year.rb

--- leap_year.rb
+++ leap_year.rb
@@ -3,3 +3,33 @@ Write a program, leap_year.rb. It will ask the user for a starting year and an e
 =end
 
 # this one is hard
+
+puts "Enter starting year: "
+start_year = gets.chomp
+start_year = start_year.to_i
+
+puts "Enter ending year: "
+end_year = gets.chomp
+end_year = end_year.to_i
+
+leap_years = []
+
+#p (start_year..end_year).to_a
+
+years = (start_year..end_year).to_a
+
+# how to do this using higher-order functions (select, map, etc.)
+
+for y in years do
+	if y % 400 == 0 then
+		leap_years.push(y)
+	end
+		
+	if y % 4 == 0 and not y % 100 == 0 then
+		leap_years.push(y)
+	end
+	
+end
+
+puts "Leap years: "
+p leap_years
